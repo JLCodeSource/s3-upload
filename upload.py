e829f405ca59af84e7611687940bea9f49513deb
JLCodeSource <JLCodeSource@gmail.com>
2023-11-24 20:17:59 +0000

    add create dir structure

--- upload.py
+++ upload.py
@@ -1,24 +1,50 @@
 import boto3
 import uuid
-import random
-import string
 import os
 import hashlib
 import base64
+import random
 
-BUF_SIZE = 65536000
+BUF_SIZE = 65536
 s3 = boto3.resource('s3')
 bucket_name = 'gb-upload'
 bucket = s3.Bucket(name=bucket_name)
 
 
 def create_temp_file(size, file_name):
-    random_file_name = ''.join([str(uuid.uuid4().hex[:6]), file_name])
-    letters = string.ascii_lowercase
-    random_content = ''.join(random.choice(letters) for i in range(size))
-    with open(random_file_name, 'w') as f:
-        f.write(str(random_content))
+    random_file_name = file_name + "-" + str(uuid.uuid4().hex[:6])
+    with open(random_file_name, 'wb') as f:
+        f.write(os.urandom(size))
     return random_file_name
+
+
+def create_temp_dir(parent):
+    random_dir_name = str(uuid.uuid4().hex[:6])
+    path = os.path.join(parent, random_dir_name)
+    os.mkdir(path)
+    return path
+
+
+def create_dir_structure(root, dirs, subs, files):
+    for i in range(dirs):
+        dir = create_temp_dir(root)
+        os.chdir(root)
+        file = "file{}".format(i)
+        create_temp_file(random.randrange(
+            1024, (10*1024*1024), 1), file)
+        for j in range(subs):
+            sub = create_temp_dir(dir)
+            path = os.path.join(root, dir)
+            os.chdir(path)
+            file = "file{}.{}".format(i, j)
+            create_temp_file(random.randrange(
+                1024, (10*1024*1024), 1), file)
+            for k in range(files):
+                path = os.path.join(root, dir, sub)
+                os.chdir(path)
+                file = "file{}.{}.{}".format(i, j, k)
+                create_temp_file(random.randrange(
+                    1024, (10*1024*1024), 1), file)
 
 
 def upload(resource, bucket, file, sha256):
@@ -44,7 +70,7 @@ def download(resource, bucket, file):
 
 
 def delete(resource, bucket, file):
-    # resource.Object(bucket, file).delete()
+    resource.Object(bucket, file).delete()
     os.remove(file)
 
 
@@ -59,17 +85,29 @@ def hash(file):
     return base64.b64encode(sha256.digest()).decode()
 
 
-file = create_temp_file(5, 'file.txt')
+def process_file(file):
+    sha256 = hash(file)
+    # sha256_hex = sha256.hexdigest()
+    print(sha256)
+    print(file)
 
-sha256 = hash(file)
-# sha256_hex = sha256.hexdigest()
-print(sha256)
-print(file)
-
-upload(s3, bucket_name, file, sha256)
-download(s3, bucket_name, file)
-# delete(s3, bucket_name, file)
+    upload(s3, bucket_name, file, sha256)
+    download(s3, bucket_name, file)
+    # delete(s3, bucket_name, file)
 
 
-for b in s3.buckets.all():
-    print(b)
+def walk(path):
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            print(os.path.join(root, name))
+        # for name in dirs:
+        #    print(os.path.join(root, name))
+
+
+# for b in s3.buckets.all():
+#    print(b)
+
+pwd = os.getcwd()
+path = os.path.join(pwd, 'tmp')
+create_dir_structure(path, 3, 2, 5)
+walk(path)
